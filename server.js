78efcc5d993dbc7c68aa651ba09f423ad545566a
MartheUwir <marthuwir@gmail.com>
2023-10-09 17:15:06 +0200

    mrn

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 import express from 'express';
 import colors from "colors";
 import dotenv from 'dotenv'
+import morgan from 'morgan';
+import connectDB from './config/db';
+
+
 
 //configuration of env
 
 dotenv.config();
+
+//database configuratioon
+connectDB();
+
 const app = express();
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
 
 app.get("/", (req, res) => {
     res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
-const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(
